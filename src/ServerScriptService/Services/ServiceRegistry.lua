68c8429a1a72fad4dbb0ab1a27de2fb42fc99b9f
B Stromblad <brandon.stromblad@gmail.com>
2026-06-23 16:13:52 -0600

    Start player profile service

--- src/ServerScriptService/Services/ServiceRegistry.lua
+++ src/ServerScriptService/Services/ServiceRegistry.lua
@@ -1,4 +1,5 @@
 local InteractionService = require(script.Parent.InteractionService)
+local PlayerProfileService = require(script.Parent.PlayerProfileService)
 local PlayerSessionService = require(script.Parent.PlayerSessionService)
 local RemoteRegistryService = require(script.Parent.RemoteRegistryService)
 local TeleportService = require(script.Parent.TeleportService)
@@ -8,6 +9,7 @@ local ServiceRegistry = {}
 
 function ServiceRegistry.start()
 	RemoteRegistryService.start()
+	PlayerProfileService.start()
 	PlayerSessionService.start()
 	InteractionService.start()
 	WorldBuilderService.build()
